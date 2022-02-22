4ac4108765a79ace24a6c7bef63a0c097963c644
Bram <bram.esendam@gmail.com>
2022-02-22 14:51:17 +0100

    Added Help command

---
+++ src/Bot.Discord/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace Bot.Discord;
+
+public class Constants
+{
+    public const string ArgsSeparator = ";";
+    public const string SlashPrefix = "/";
+    
+    public static class Colors
+    {
+        public static readonly Color Error = Color.Red;
+        public static readonly Color Successful = Color.LawnGreen;
+        public static readonly Color Neutral = Color.White;
+    }
+}
