8cf7957288076caa6566e8f4d1fc4457fc0abba7
Bram <bram.esendam@gmail.com>
2022-02-22 15:10:53 +0100

    Added domain models

---
+++ src/persistence/Bot.persistence.Domain/BaseDocument.cs
@@ -0,0 +1,36 @@
+﻿namespace Bot.persistence.Domain;
+
+/// <summary>
+///     This class represents a basic document that can be stored in MongoDb.
+/// </summary>
+public class BaseDocument
+{
+    /// <summary>
+    ///     Creates a new <see cref="BaseDocument" />
+    /// </summary>
+    public BaseDocument()
+    {
+        AddedAtUtc = DateTime.UtcNow;
+        BsonObjectId = string.Empty;
+    }
+
+    /// <summary>
+    ///     Creates a new <see cref="BaseDocument" />
+    /// </summary>
+    /// <param name="bsonObjectId">The object id that will be used for the document.</param>
+    public BaseDocument(string bsonObjectId)
+    {
+        AddedAtUtc = DateTime.UtcNow;
+        BsonObjectId = bsonObjectId;
+    }
+
+    /// <summary>
+    ///     The object id of the document.
+    /// </summary>
+    public string BsonObjectId { get; set; }
+
+    /// <summary>
+    ///     The <see cref="DateTime" /> of when the <see cref="BaseDocument" /> was added to the collection.
+    /// </summary>
+    public DateTime AddedAtUtc { get; set; }
+}
---
+++ src/persistence/Bot.persistence.Domain/Models/Request.cs
@@ -0,0 +1,45 @@
+﻿namespace Bot.persistence.Domain.Models;
+
+public class Request : BaseDocument
+{
+    /// <summary>
+    ///     Used command.
+    /// </summary>
+    public string? Command { get; set; }
+
+    /// <summary>
+    ///     Error message.
+    ///     Note: This is null if the request is successful.
+    /// </summary>
+    public string? ErrorMessage { get; set; }
+
+    /// <summary>
+    ///     <see cref="bool" /> for if the request was successful.
+    /// </summary>
+    public bool IsSuccessful { get; set; }
+
+    /// <summary>
+    ///     TimeStamp for then the request happened.
+    /// </summary>
+    public DateTime TimeStamp { get; set; }
+
+    /// <summary>
+    ///     The message id of the request.
+    /// </summary>
+    public ulong? MessageId { get; set; }
+
+    /// <summary>
+    ///     The server id of where the command was used.
+    /// </summary>
+    public ulong ServerId { get; set; }
+
+    /// <summary>
+    ///     The user id of the user that used the command.
+    /// </summary>
+    public ulong UserId { get; set; }
+
+    /// <summary>
+    ///     The amount of time the request took to execute.
+    /// </summary>
+    public long? ElapsedMilliseconds { get; set; }
+}
