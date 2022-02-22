71604bdaf6cd2613cc5a055f721708543574c55a
Bram <bram.esendam@gmail.com>
2022-02-22 15:11:15 +0100

    Added REPOs and UNITOFWORKs interfaces

---
+++ src/persistence/Bot.persistence/Repositories/IRequestsRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bot.persistence.Domain.Models;
+
+namespace Bot.persistence.Repositories;
+
+/// <summary>
+///     This interface holds all the extra methods to query to the <see cref="Request" /> table/collection.
+/// </summary>
+public interface IRequestsRepository : IRepository<Request>
+{
+}
