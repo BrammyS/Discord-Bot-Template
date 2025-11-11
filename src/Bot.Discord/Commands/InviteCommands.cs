1328d7d3e73ce43484e77828d07868800ccb52ad
BrammyS <bram.esendam@gmail.com>
2025-11-11 20:54:25 +0100

    Updates to .NET 10
    Updates the project to .NET 10.
    
    Includes updating the target framework, .NET SDK version in the build workflow, and relevant package versions.
    
    Updates Color-Chan.Discord package to v5 and adapts component builder syntax.

--- src/Bot.Discord/Commands/InviteCommands.cs
+++ src/Bot.Discord/Commands/InviteCommands.cs
@@ -1,6 +1,7 @@
 ﻿using Color_Chan.Discord.Commands.Attributes;
 using Color_Chan.Discord.Commands.Attributes.ProvidedRequirements;
 using Color_Chan.Discord.Commands.MessageBuilders;
+using Color_Chan.Discord.Commands.MessageBuilders.Components;
 using Color_Chan.Discord.Commands.Modules;
 using Color_Chan.Discord.Core.Common.API.DataModels;
 using Color_Chan.Discord.Core.Common.Models;
@@ -18,9 +19,14 @@ public class InviteCommands : SlashCommandModule
     public const string InviteCommandName = "invite";
     public const string InviteCommandDesc = "Invite the bot somewhere else!";
 
-    public static readonly IDiscordComponent InviteMe =
+    private static readonly IDiscordComponent InviteMe =
         new ActionRowComponentBuilder()
-            .WithButton("Invite me!", DiscordButtonStyle.Link, null, $"https://discord.com/api/oauth2/authorize?client_id={Constants.BotId}&permissions=0&scope=bot%20applications.commands")
+            .WithSubComponent(
+                new ButtonComponentBuilder()
+                    .WithLabel("Invite me!")
+                    .WithUrl($"https://discord.com/api/oauth2/authorize?client_id={Constants.BotId}&permissions=0&scope=bot%20applications.commands")
+                    .WithStyle(DiscordButtonStyle.Link)
+            )
             .Build();
 
     /// <summary>
@@ -33,17 +39,17 @@ public class InviteCommands : SlashCommandModule
     public Task<Result<IDiscordInteractionResponse>> InviteAsync()
     {
         var inviteMeEmbed = new DiscordEmbedBuilder()
-                            .WithTitle("Invite me")
-                            .WithDescription("I need more friends, add me to other Discord servers!")
-                            .WithColor(Constants.Colors.Successful)
-                            .WithTimeStamp()
-                            .Build();
+            .WithTitle("Invite me")
+            .WithDescription("I need more friends, add me to other Discord servers!")
+            .WithColor(Constants.Colors.Successful)
+            .WithTimeStamp()
+            .Build();
 
         // Build the embedded response.
         var inviteResponse = new InteractionResponseBuilder()
-                             .WithEmbed(inviteMeEmbed)
-                             .WithComponent(InviteMe)
-                             .Build();
+            .WithEmbed(inviteMeEmbed)
+            .WithComponent(InviteMe)
+            .Build();
 
         //  Return the response to Discord.
         return Task.FromResult(FromSuccess(inviteResponse));
